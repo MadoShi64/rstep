5acd54f7f6f6530c9686a4c8317ed495ab781817
Yélognissè Agbohessou <72917844+MadoShi64@users.noreply.github.com>
2022-03-08 11:44:49 +0000

    2022-03-08

--- R/step_plot.R
+++ R/step_plot.R
@@ -46,49 +46,28 @@ step_plot = function(dataframe,
 
       var=names(dataframe[c(varname,paste0(varname,".simu"))])
 
-      if (sum(is.na(dataframe[2])) < nrow(dataframe[3])/1.5){
-        df2 <- dataframe %>%
-          select(Date,all_of(var)) %>%
-          gather(key = "variable", value = "value", -Date)
-        ggplot(df2, aes(x = ymd(Date), y=value)) +
-          geom_line(aes(color = variable), size = 0.5)+
-          scale_color_manual(labels = c("Observation","Simulation"),
-                             values = c("black","peru")) +
-          scale_x_date(date_labels="%Y",date_breaks  ="1 year")+
-          ylab(names(dataframe[varname]))+xlab(names(dataframe[1]))+
-          theme_bw() + # eliminate default background
-          theme(panel.grid.major = element_blank(), # eliminate major grids
-                axis.text.x = element_text(size = 10, face = "bold", color="black"),
-                axis.title.y = element_text(size = 10, face = "bold", color="black"),
-                panel.grid.minor = element_blank(), # eliminate minor grids
-                text = element_text(family="serif"),
-                axis.title.x = element_text(size = 10, face = "bold", color="black"),
-                legend.position = "bottom",
-                legend.title = element_blank()
-          )
+      df2 <- dataframe %>%
+        select(Date,all_of(var)) %>%
+        gather(key = "variable", value = "value", -Date)
+      ggplot(df2, aes(x=ymd(Date),y=value,colour=variable))+
+        geom_line(data=subset(df2,variable == paste0(varname,".simu")),linetype = "solid", size = 0.8)+
+        geom_point(data=subset(df2,variable == varname))+
+        scale_color_manual(labels = c("Observation","Simulation"),
+                           values = c("black","peru")) +
+        #scale_x_date(date_labels="%Y",date_breaks  ="1 year")+
+        ylab(varname)+xlab("Date")+
+        theme_bw() + # eliminate default background
+        theme(panel.grid.major = element_blank(), # eliminate major grids
+              axis.text.x = element_text(size = 10, face = "bold", color="black"),
+              axis.title.y = element_text(size = 10, face = "bold", color="black"),
+              panel.grid.minor = element_blank(), # eliminate minor grids
+              text = element_text(family="serif"),
+              axis.title.x = element_text(size = 10, face = "bold", color="black"),
+              legend.position = "bottom",
+              legend.title = element_blank()
+        )
 
-      }else{
-        df2 <- dataframe %>%
-          select(Date,all_of(var)) %>%
-          gather(key = "variable", value = "value", -Date)
-        ggplot(df2, aes(x=ymd(Date),y=value,colour=variable))+
-          geom_line(data=subset(df2,variable == paste0(varname,".simu")),linetype = "solid", size = 0.8)+
-          geom_point(data=subset(df2,variable == varname))+
-          scale_color_manual(labels = c("Observation","Simulation"),
-                             values = c("black","peru")) +
-          scale_x_date(date_labels="%Y",date_breaks  ="1 year")+
-          ylab(varname)+xlab("Date")+
-          theme_bw() + # eliminate default background
-          theme(panel.grid.major = element_blank(), # eliminate major grids
-                axis.text.x = element_text(size = 10, face = "bold", color="black"),
-                axis.title.y = element_text(size = 10, face = "bold", color="black"),
-                panel.grid.minor = element_blank(), # eliminate minor grids
-                text = element_text(family="serif"),
-                axis.title.x = element_text(size = 10, face = "bold", color="black"),
-                legend.position = "bottom",
-                legend.title = element_blank()
-          )
-      }
+
     }else{
       df2 <- df %>%
         select(Date,all_of(varname)) %>%
@@ -96,7 +75,7 @@ step_plot = function(dataframe,
       ggplot(df2, aes(x = ymd(Date), y=value)) +
         geom_line(aes(color = variable), size = 0.65)+
         scale_color_manual(values="peru")+
-        scale_x_date(date_labels="%Y",date_breaks  ="1 year")+
+        #scale_x_date(date_labels="%Y",date_breaks  ="1 year")+
         ylab(names(dataframe[varname]))+xlab("Date")+
         theme_bw() + # eliminate default background
         theme(panel.grid.major = element_blank(), # eliminate major grids
